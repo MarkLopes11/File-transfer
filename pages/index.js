3d301aaf0e31f369de89ab732d5f587b29177a7a
Mark Lopes <mark157898@gmail.com>
2025-09-10 23:14:14 +0530

    adsense

--- pages/index.js
+++ pages/index.js
@@ -7,5 +7,6 @@ function Home() {
     </div>
   );
 }
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8546018790197410"
+     crossorigin="anonymous"></script>
 export default Home;
